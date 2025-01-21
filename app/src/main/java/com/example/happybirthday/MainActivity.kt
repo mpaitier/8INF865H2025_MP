a5d8c9b1aa234a6e855969cd17013d631470b883
PAITIER Mathias <mpaitier44@gmail.com>
2025-01-21 10:17:20 -0500

    7 - Ajouter un autre élément textuel

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -25,11 +25,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
         fontSize = 100.sp,
         lineHeight = 116.sp,
+    )
+    Text(
+        text = from,
+        fontSize = 36.sp,
     )
 }
 
@@ -37,7 +41,7 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Goustan!")
+        GreetingText(message = "Happy Birthday Goustan!", from ="From Mathias")
     }
 }
 
